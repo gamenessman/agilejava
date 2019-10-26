82274f3144ddc6ed73be31cd51e1dc8a758d5ea9
klman <klman@qq.com>
2019-10-27 02:25:19 +0800

    aaa

--- src/test/java/com/agilejava/one/FizzbuzzTest.java
+++ src/test/java/com/agilejava/one/FizzbuzzTest.java
@@ -8,26 +8,26 @@ import static org.junit.Assert.assertThat;
 public class FizzbuzzTest {
     @Test
     public void should_say_1_when_valve_is_1() {
-        int actualvue = 1;
-        String expectedWord = "1";
-        assertGameNumber(expectedWord, actualvue);
+        assertGameNumber(1, "1");
     }
+
     //当数字被3整除，显示为fizz
     @Test
     public void should_say_fizz() {
-        assertGameNumber("fizz", 3);
+        assertGameNumber(3, "fizz");
     }
 
     @Test
     public void should_say_buzz() {
-        assertGameNumber("buzz", 5);
+        assertGameNumber(5, "buzz");
     }
 
     @Test
     public void should_say_fizzbuzz() {
-        assertGameNumber("fizzbuzz", 15);
+        assertGameNumber(15, "fizzbuzz");
     }
-    private void assertGameNumber(String expectedWord, int actualvue) {
+
+    private void assertGameNumber(int actualvue, String expectedWord) {
         GameNamber gameNamber = new GameNamber(actualvue);
         assertThat(gameNamber.say(), is(expectedWord));
     }
